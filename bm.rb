2d05eeeeb3bf52bd8ae4dee1dfceef20af0f4108
Keishi wada <kcwebapply@gmail.com>
2019-05-01 21:16:05 +0900

    Update bm.rb

--- bm.rb
+++ bm.rb
@@ -6,7 +6,7 @@ class Bm < Formula
   desc "manage bookmark easily on terminal"
   homepage "https://github.com/kcwebapply/bm"
   url "https://github.com/kcwebapply/bm/archive/1.0.0.tar.gz"
-  sha256 "6be2b77365f2ae3cad5a4af58fbdc03728fbaaeff821114cec25c29f3a94ddbd"
+  sha256 "98d44f17b4b7f308567052e4ce829df098f6fee714b568c25d136661f6a6d620"
 
   depends_on "dep" => :build
   depends_on "go" => :build
